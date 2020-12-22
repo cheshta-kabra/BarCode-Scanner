3dea4e2d55a47b7bc4d9a0dc579d5176159fd818
cheshta-kabra <64300181+cheshta-kabra@users.noreply.github.com>
2020-12-22 16:25:45 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -3,8 +3,17 @@ import ScannerScreen from './screens/scannerScreen'
 import HomeScreen from './screens/homeScreen'
 import {createAppContainer} from 'react-navigation'
 import {createBottomTabNavigator} from 'react-navigation-tabs'
+import{Image, View} from 'react-native'
 export default class App extends React.Component{
   render(){
+        <Image
+          style={{width: 150,
+            height: 150,
+            marginLeft: 95,}}
+          source={{
+            uri:'D:\cheshta(WhiteHat jr)\Apps\Projects\Barcode Scanner\assets\barcodeScanner.jpg'
+          }}
+        />
     return (
       <AppContainer />
     );
